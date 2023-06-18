7761aee929a00bd0e026e7a8cdac3ae690f0e6e5
CaptiosusNomen <asteelscholar@gmail.com>
2023-06-18 06:53:10 -0600

    Changed sys.platform to sys.platform.startswith

--- BotMain.py
+++ BotMain.py
@@ -3,22 +3,19 @@ from discord.ext import commands
 import os
 import sys
 
-
-
-
 FilePath = os.path.dirname(os.path.abspath(__file__))
 intents = discord.Intents.all()
 Bot = commands.Bot(command_prefix=["A*"], intents=intents, case_insensitive=True)
 Bot.remove_command('help')
 
 async def LoadCogs():
-    if sys.platform == "linux" or sys.platform == "linux2":
+    if sys.platform.startswith("lin"):
         for file in os.listdir(f"{FilePath}/Cogs"):
             if file.endswith("Cog.py"):
                 await Bot.load_extension(f'Cogs.{file[:-3]}')
                 print(f"{file} Loaded")
 
-    if sys.platform == "win32" or sys.platform == "win64":
+    if sys.platform.startswith("win"):
         for file in os.listdir(f"{FilePath}\\Cogs"):
             if file.endswith("Cog.py"):
                 await Bot.load_extension(f'Cogs.{file[:-3]}')
@@ -31,14 +28,5 @@ async def on_ready():
     print(f'Client Name: {Bot.user.name}')
     print("--------------------")
 
-
-
-
-@Bot.event
-async def on_resumed():
-    pass
-
-
 from TokenHolder import Token
-Token = Token()
-Bot.run(Token)
+Bot.run(Token())
